9f7ace889507e94d7bb9e4e8c85cfaae16d83dd6
tinuh <tinuvanapamula@gmail.com>
2022-02-09 14:16:43 -0500

    make non-monetary sponsors smaller, and add animation

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -7,33 +7,75 @@ import {
   Wrap,
   WrapItem,
   Tooltip,
-  Box
+  Box,
+  ModalContent
 } from "@chakra-ui/react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 import Button from "./Button";
 import Link from "./Link";
 import sponsors from '../data/sponsors';
 
 export default function Sponsors() {
+  const control = useAnimation();
+  const {ref, inView} = useInView();
+
+  React.useEffect(() => {
+    if (inView) {
+      control.start('visible');
+    }
+  }, [control, inView]);
+
+  const variants = {
+    visible: {
+        scale: 1,
+        opacity: 1,
+        transition: {
+             duration: 1,
+         },
+    },
+  };
+
   return (
     <Container maxW="container.lg" px={8} py={4} align="center">
       <Heading as="h2" size="xl">
         Sponsors
       </Heading>
+      <motion.div
+        ref = {ref}
+        initial={{scale: 0.9, opacity: 0}}
+        animate={control}
+        variants={variants}
+      >
+        <Wrap justify="center">
+          {sponsors.monetary.map(({name, url, img}) => (   
+            <WrapItem p = {3} w= "200px" justifyContent="center" alignItems="center">   
+              <Tooltip hasArrow label={name}>
+                <Box p = {2}>
+                  <Link href = {url} isExternal>
+                    <Image w = "100%" src = {`/assets/sponsorLogos/${img}`}/>
+                  </Link>  
+                </Box>
+              </Tooltip>
+            </WrapItem>
+          ))}
+        </Wrap>
 
-      <Wrap justify="center">
-        {sponsors.map(({name, url, img}) => (   
-          <WrapItem p = {3} w= "200px" justifyContent="center" alignItems="center">   
-            <Tooltip hasArrow label={name}>
-              <Box p = {2}>
-                <Link href = {url} isExternal>
-                  <Image w = "100%" src = {`/assets/sponsorLogos/${img}`}/>
-                </Link>  
-              </Box>
-            </Tooltip>
-          </WrapItem>
-        ))}
-      </Wrap>
+        <Wrap justify="center">
+          {sponsors.others.map(({name, url, img}) => (   
+            <WrapItem p = {3} w= "150px" justifyContent="center" alignItems="center">   
+              <Tooltip hasArrow label={name}>
+                <Box p = {2}>
+                  <Link href = {url} isExternal>
+                    <Image w = "100%" src = {`/assets/sponsorLogos/${img}`}/>
+                  </Link>  
+                </Box>
+              </Tooltip>
+            </WrapItem>
+          ))}
+        </Wrap>
+      </motion.div>
 
       <Text mt={3}>
 				Help us make this hackathon possible! Email us at <Link href="mailto:team@blairhacks.com">team@blairhacks.com</Link>.
--- data/sponsors.js
+++ data/sponsors.js
@@ -1,24 +1,25 @@
-export default [
-	{
+export default {
+	monetary : [{
 		name: "Magnet Foundation",
 		url: "https://www.mbhsmagnet.org",
-		img: "magnet_foundation.png"
+		img: "magnet_foundation.png",
 	},
 	{
 		name: "Mindpoint Group",
 		url: "https://www.mindpointgroup.com",
-		img: "mindpoint.png"
+		img: "mindpoint.png",
 	},
 	{
 		name: "Dragos",
 		url: "https://dragos.com",
-		img: "dragos.png"
+		img: "dragos.png",
 	},
 	{
 		name: "RStudio",
 		url: "https://www.rstudio.com",
-		img: "rstudio.png"
-	},
+		img: "rstudio.png",
+	}],
+	others: [
 	{
 		name: "Digital Ocean",
 		url: "https://digitalocean.com",
@@ -27,16 +28,16 @@ export default [
 	{
 		name: "Sticker Giant",
 		url: "https://stickergiant.com",
-		img: "stickergiant.png"
+		img: "stickergiant.png",
 	},
 	{
 		name: "Wolfram",
 		url: "https://www.wolfram.com",
-		img: "wolfram.png"
-	},
+		img: "wolfram.png",
+	},	
 	{
 		name: "Leading Learners",
 		url: "https://linktr.ee/leadinglearners",
-		img: "leading-learners.png"
-	}
-]
+		img: "leading-learners.png",
+	}]
+}
