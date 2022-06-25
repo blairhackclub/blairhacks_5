784632b581af01f59501dddfa5876652f8505ef6
tinuvanapamula@gmail.com <tinuvanapamula@gmail.com>
2022-06-25 15:56:36 -0400

    update meta data

--- pages/_app.js
+++ pages/_app.js
@@ -12,10 +12,10 @@ function MyApp({ Component, pageProps }) {
         <title>BlairHacks_6</title>
         <link rel="icon" href="/favicon.ico" />
         
-        <meta name="description" content="A free, 24-hour hackathon for DMV high school students hosted annually by MBHS students in Silver Spring, MD. Join us for BlairHacks_5 in February 2022!"/>
-        <meta property="og:description" content="A free, 24-hour hackathon for DMV high school students hosted annually by MBHS students in Silver Spring, MD. Join us for BlairHacks_5 in February 2022!" key="ogdesc"/>
+        <meta name="description" content="A free, 24-hour hackathon for DMV high school students hosted annually by MBHS students in Silver Spring, MD. Join us for BlairHacks_6 in February 2023!"/>
+        <meta property="og:description" content="A free, 24-hour hackathon for DMV high school students hosted annually by MBHS students in Silver Spring, MD. Join us for BlairHacks_6 in February 2023!" key="ogdesc"/>
         <meta property="og:image" content="/assets/logo.png" key="ogimage"/>
-        <meta property="og:title" content="BlairHacks_5" key="ogtitle"/>
+        <meta property="og:title" content="BlairHacks_6" key="ogtitle"/>
         <meta property="og:site_name" content="BlairHacks" key="ogsitename"/>
         <meta name='theme-color' content="#141414"/>
       </Head>
