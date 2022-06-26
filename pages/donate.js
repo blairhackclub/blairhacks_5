0991ebf8f488b0a97c59fde42a6362368de84ddc
tinuvanapamula@gmail.com <tinuvanapamula@gmail.com>
2022-06-26 00:28:26 -0400

    donate page

---
+++ pages/donate.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function donates() {
+  return (
+    <iframe src="https://bank.hackclub.com/donations/start/blairhacks" style = {{height: "100vh"}} name="donateFrame" scrolling="yes" frameBorder="0" marginHeight="0px" marginWidth="0px" width="100%" allowFullScreen></iframe>
+  )
+}
